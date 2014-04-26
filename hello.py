92b94ff70563bbf673a0c4b5494a9ac5452ab1a0
Marcus Kemper <kemper.mt@gmail.com>
2014-04-26 16:38:41 +0200

    added template file

--- hello.py
+++ hello.py
@@ -4,5 +4,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
+
 def hello():
     return 'Hello World!'
+
+def index():
+
+    return render_template("index.html,
+        title = 'TestApp',
+        user = 'Gustave Gans')
