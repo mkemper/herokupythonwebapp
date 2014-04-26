14d665a2ac7517556f6e5ef6c8890b96919eda29
Marcus Kemper <kemper.mt@gmail.com>
2014-04-26 21:20:33 +0200

    next try

--- hello.py
+++ hello.py
@@ -1,15 +1,12 @@
 import os
+
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/index')
+@app.route('/hello')
 
 def hello():
-    return 'Hello World!'
-
-def index():
-
-    return render_template("index.html,title = 'TestApp',user = 'Gustave Gans'")
+    return render_template("index.html",title = 'TestApp',user = 'Gustave Gans')
