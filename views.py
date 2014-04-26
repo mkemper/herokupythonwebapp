b6144a84597e2d6655e5978afa62bc82e7fe0215
Marcus Kemper <kemper.mt@gmail.com>
2014-04-26 23:12:17 +0200

    Gute Nacht

--- views.py
+++ views.py
@@ -1,12 +1,27 @@
+#-*- coding: utf-8 -*-
+
+"""
+Testapp for Flask and Heroku
+2014/04/26
+Marcus Kemper
+"""
+
 import os
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
+# create the application
 app = Flask(__name__)
 
+
+# setting the route for root and 'hello'
 @app.route('/')
 @app.route('/hello')
 
+# corresponding function
 def hello():
     return render_template("index.html",title = 'TestApp',user = 'Gustave Gans')
+
+@app.route('/hullo')
+def hullo():
+    return "Grsldmpf!"
