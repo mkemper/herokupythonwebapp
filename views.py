55e76253ac2ae346e8958a00792b50c2820ac7a9
Marcus Kemper <kemper.mt@gmail.com>
2014-05-02 21:54:06 +0200

    Nachtrag config.py und views.py

--- views.py
+++ views.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
 """
@@ -9,10 +10,14 @@ Marcus Kemper
 import os
 
 from flask import Flask, render_template
+import test
+from forms import LoginForm
 
 # create the application
 app = Flask(__name__)
 
+# use config.py
+app.config.from_object('config')
 
 # setting the route for root and 'hello'
 @app.route('/')
@@ -20,8 +25,15 @@ app = Flask(__name__)
 
 # corresponding function
 def hello():
-    return render_template("index.html",title = 'TestApp',user = 'Gustave Gans')
+    return render_template("index.html",title = 'TestApp',user = 'General Gustave Gans')
 
 @app.route('/hullo')
 def hullo():
     return "Grsldmpf!"
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Sign In', form = form)
+
